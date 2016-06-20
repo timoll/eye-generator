4faebdd624432a71cae383cb6903aa5634961b7f
Timothee Mollet <timothee.mollet@gmail.com>
2016-06-20 09:57:46 +0200

    include some animations and complete calibration

--- videos/generate_csv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./starburst -v rightup.mp4 > csv/rightup.csv
-./starburst -v rightdown.mp4 > csv/rightdown.csv
-./starburst -v leftup.mp4 > csv/leftup.csv
-./starburst -v leftdown.mp4 > csv/leftdown.csv
